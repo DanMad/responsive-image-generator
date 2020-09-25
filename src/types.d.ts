8d1c1b241667ecfc631706becea58949e506f678
Daniel Maddison <dmaddison@icloud.com>
2020-09-25 18:19:58 +1000

    Updated Responsive Image Generator's types

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,70 +10,18 @@ declare interface String {
   trim(): string;
 }
 
-interface Breakpoints {
-  l: string;
-  m: string;
-  s: string;
-  xl: string;
-  xs: string;
-}
+type AssetContext = `l` | `m` | `s` | `xl` | `xs`;
 
-interface Asset {
-  args: AssetArgs;
-  index: number;
-  layerId: number;
-}
+type RequiredAssetParam = `ext` | `name`;
 
-interface AssetArgPatterns {
-  context: RegExp;
-  def: RegExp;
-  ext: RegExp;
-  qual: RegExp;
-  size: RegExp;
-}
+type OptionalAssetParam = `context` | `def` | `qual` | `size`;
 
-interface AssetArgs {
-  context?: string;
-  def?: string;
-  ext: string;
-  name: string;
-  qual?: string;
-  size?: string;
-}
+type AssetParam = RequiredAssetParam | OptionalAssetParam;
 
-type AssetParam = `context` | `def` | `ext` | `qual` | `size`;
-type Assets = Asset[];
+type Asset = { [key in RequiredAssetParam]: string } &
+  { [key in OptionalAssetParam]?: string };
 
-interface Context {
-  assets: Assets;
-  maxWidth?: string;
-}
-
-interface Contexts {
-  l?: Context;
-  m?: Context;
-  s?: Context;
-  unset?: Context;
-  xl?: Context;
-  xs?: Context;
-}
-
-type TShirtSize = `l` | `m` | `s` | `unset` | `xl` | `xs`;
-type TShirtSizes = TShirtSize[];
-
-interface TempImage {
-  alt: string;
-  compress: boolean;
-  contexts: Contexts;
-  name: string;
-  srcDir: string;
-}
-
-interface EditTexts {
-  l?: EditText;
-  m?: EditText;
-  s?: EditText;
-  unset?: EditText;
-  xl?: EditText;
-  xs?: EditText;
-}
+type Assets = {
+  contexts: Partial<Record<AssetContext, Asset[]>>;
+  layers: Layer[];
+};
